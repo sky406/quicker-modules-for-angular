f0bfb8c590ded5e7899815619411ea7ae2623293
wesley <wesleynworisa@gmail.com>
2022-04-07 15:18:07 -0700

    fully implemented comparedate abd the test function

--- granular date comparison/interfaces/daycomp_result.interface.ts
+++ granular date comparison/interfaces/daycomp_result.interface.ts
@@ -2,10 +2,11 @@ export interface dm_result
 { 
     year_dif:number,
     month_dif:number,
-    day_dif:number,
+    date_dif:number
     hour_dif:number,
     min_dif:number,
     sec_dif:number,
     mil_dif:number,
-    before:boolean
+    before:boolean,
+    difference:number
 }
