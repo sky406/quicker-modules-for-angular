86970859dcb268f1aeabcdcaa319c88c08baf34d
wesley <wesleynworisa@gmail.com>
2022-04-07 13:37:22 -0700

    added day month result interface

---
+++ granular date comparison/interfaces/daycomp_result.interface.ts
@@ -0,0 +1,11 @@
+export interface dm_result
+{ 
+    year_dif:number,
+    month_dif:number,
+    day_dif:number,
+    hour_dif:number,
+    min_dif:number,
+    sec_dif:number,
+    mil_dif:number,
+    before:boolean
+}
